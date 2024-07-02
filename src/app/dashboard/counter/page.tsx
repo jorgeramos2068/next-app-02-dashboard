a3bf349f5939f7cb1b38bf703747922a7647f3e8
Jorge Ramos <jorgeramos2068@gmail.com>
2024-07-02 13:04:23 -0600

    feat: reorganize directories structure

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Metadata } from 'next';
 
-import { CartCounter } from '@/components/shopping-cart';
+import { CartCounter } from '@/app/dashboard/counter/_components/shopping-cart';
 
 export const metadata: Metadata = {
   title: 'Shopping Cart',
