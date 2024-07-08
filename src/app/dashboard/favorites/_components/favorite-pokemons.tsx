45593b716fe4bd098310a71d8724be515cd9ded7
Jorge Ramos <jorgeramos2068@gmail.com>
2024-07-08 08:49:49 -0600

    feat: create no favorites component

--- src/app/dashboard/favorites/_components/favorite-pokemons.tsx
+++ src/app/dashboard/favorites/_components/favorite-pokemons.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import * as React from 'react';
+
 import { PokemonGrid } from '@/components/pokemons';
 import { useAppSelector } from '@/stores';
+import { NoFavorites } from './no-favorites';
 
 export const FavoritePokemons = () => {
   const favoritePokemons = useAppSelector(store => Object.values(store.pokemons));
+  const [pokemons, setPokemons] = React.useState(favoritePokemons);
 
-  return <PokemonGrid pokemons={favoritePokemons} />;
+  return <>{pokemons.length > 0 ? <PokemonGrid pokemons={pokemons} /> : <NoFavorites />}</>;
 };
